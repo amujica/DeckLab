50ac73926269c3407df858d8d35f5ded3e1ae64b
amujica <albertomujica97@gmail.com>
2020-01-18 16:05:07 +0100

    Navbar and main routes

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+//var favicon = require('serve-favicon');
+const { format } = require('timeago.js');
+
+const path = require('path');
+
+// intializations
+const app = express();
+require('./database');
+
+// settings
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 3000);
+
+// middlewares
+
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: false}));
+//app.use(favicon(path.join(__dirname, 'public/img', 'favicon.ico')));
+// Global variables
+app.use((req, res, next) => {
+    app.locals.format = format;
+    next();
+});
+
+// routes
+app.use(require('./routes/index'));
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// start
+app.listen(3000, () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
---
+++ src/routes/index.js
@@ -0,0 +1,36 @@
+const { Router } = require('express');
+
+const path = require('path');
+
+const router = Router();
+
+
+
+router.get('/', (req, res) => {
+    
+    res.render('index');
+});
+
+router.get('/lab', (req, res) => {
+    
+    res.render('lab');
+});
+
+
+router.get('/algorithm', (req, res) => {
+    
+    res.render('algorithm');
+});
+
+
+router.get('/about', (req, res) => {
+    
+    res.render('about');
+});
+
+
+router.get('/profile', (req, res) => {
+    
+    res.render('profile');
+});
+module.exports = router
