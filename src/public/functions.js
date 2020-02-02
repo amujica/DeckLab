95c93a42762e3a11e7a4f0f122cb6c00fd502989
amujica <albertomujica97@gmail.com>
2020-02-02 15:27:23 +0100

    Starting to design functions

--- src/public/functions.js
+++ src/public/functions.js
@@ -1,13 +1,23 @@
 
 var divCurr = $('#currentComb');
 
+$(function () {
+  $('#invertir').on('click', function () {
+    divCurr.text(divCurr.text() + "--> Invertir");
+    //Esto es en verdad poner carta n en m! con n=1 y m=2. Desarrollar eso con un selector
+    var num = $('#invertidas').val();
+    $( ".naipes div:nth-child(1)" ).before( $( ".naipes div:nth-child(2)" ) );
+    console.log(num-1)
+  });
+});
+
 
 $(function () {
     $('#faroExt').on('click', function () {
       divCurr.text(divCurr.text() + " --> Faro Exterior");
 
-      $( "#naipes div:nth-child(1)" ).before( $( "#naipes div:nth-child(2)" ) );
-      
+      $( ".naipes div:nth-child(1)" ).before( $( ".naipes div:nth-child(2)" ) );
+      //Esto es en verdad poner carta n en m! con n=1 y m=2. Desarrollar eso con un selector
 
     });
   });
@@ -28,10 +38,34 @@ $(function () {
    $('#cut').on('click', function () {
     divCurr.text(divCurr.text() + " --> Cortar por 3");
 
-    $( "#naipes div:nth-child(3)" ).before( $( "#naipes div:nth-child(6)" ) );
+    $( ".naipes div:nth-child(3)" ).before( $( ".naipes div:nth-child(50)" ) );
 
    });
  });
 
+ $(function () {
+  $('#shuffle').on('click', function () {
+    
+    $(".naipes").each(function(){
+      var divs = $(this).find('div');
+      console.log(divs)
+      for(var i = 0; i < divs.length; i++) $(divs[i]).remove();   //Borra el elemento del DOM    
+          
+      //the fisher yates algorithm, from http://stackoverflow.com/questions/2450954/how-to-randomize-a-javascript-array
+      var i = divs.length;
+      if ( i == 0 ) return false;
+      while ( --i ) {
+         var j = Math.floor( Math.random() * ( i + 1 ) );
+         var tempi = divs[i];
+         var tempj = divs[j];
+         divs[i] = tempj;
+         divs[j] = tempi;
+         
+       }
+      for(var i = 0; i < divs.length; i++) $(divs[i]).appendTo(this);
+  });   
 
+  });
+ });
 
+ 
