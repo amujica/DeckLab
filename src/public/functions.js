4358b71f89e905212be3eb937da6c522c47bd7f0
amujica <albertomujica97@gmail.com>
2020-02-19 22:59:09 +0100

    Disable buttons properly and antifaros

--- src/public/functions.js
+++ src/public/functions.js
@@ -3,184 +3,6 @@ var divCurr = $('#currentComb');
 var divOrigen = $('#stackOrigen');
 var divDestino = $('#stackDestino');
 
-$(function () {
-  $('#invertir').on('click', function () {
-    var num = $(".ui-selected .naipeImg").length
-    
-    
-
-    console.log(num)
-    var lim = Math.floor(num/2);
-    divCurr.text(divCurr.text() + " --> Inverse " + num + " cards");
-
-    $( ".naipe" ).each(function(index,element) {
-      if (index < lim ){
-        index1 = index + 1
-        $( ".tapete div:nth-child(" + num + ")" ).before( $( ".tapete div:nth-child(" + index1 + ")" ) );
-        $( ".tapete div:nth-child(" + index1 + ")" ).before( $( ".tapete div:nth-child(" + num + ")" ) );
-        num--  
-      }
-    })
-    
-    setTimeout(() => {  
-      $(".ui-selected").each(function(index,element) {
-        element.classList.remove("ui-selected");
-      })
-     }, 1500);
-    
-     $('#cut').prop("disabled",true);
-     $('#invertir').prop("disabled",true);
-     $('#faroExtPar').prop("disabled",true);
-     $('#faroIntPar').prop("disabled",true);
-     $('#faroExt').prop("disabled",false);
-    $('#faroInt').prop("disabled",false);
-    checkCurrDeck()
-  });
-});
-
-
-
-
-$(function () {
-   $('#clear').on('click', function () {
-     divCurr.text("");
-   });
- });
- 
-
-
-/* $(function () {
-  $('#cut').on('click', function () {
-    var num = parseInt($('#cortadas').val());
-    
-    divCurr.text(divCurr.text() + " --> Cut in card " + num);
-
-    $( ".naipe" ).each(function(index,element) {
-      console.log(index)
-      if(index<num){
-        $( ".naipes div:nth-child(1)" ).insertAfter( $( ".naipes div:nth-child(52)" ) );
-        
-      }
-    })
-  });
-});*/
-
-$(function () {
-  $('#change').on('click', function () {
-    var num = parseInt($('#cambiadas').val());
-    var num1 = parseInt($('#cambiadas1').val()) + 1;
-
-    
-    divCurr.text(divCurr.text() + " --> Put card " + num + " in position " + num1);
-
-    $( ".tapete div:nth-child(" + num1 + ")" ).before( $( ".tapete div:nth-child(" + num + ")" ) );
-    
-    
-    
-  });
-});
-
-
- $(function () {
-  $('#shuffle').on('click', function () {
-    
-    $(".tapete").each(function(){
-      var divs = $(this).find('div');
-      console.log(divs)
-      for(var i = 0; i < divs.length; i++) $(divs[i]).remove();   //Borra el elemento del DOM    
-          
-      //the fisher yates algorithm, from http://stackoverflow.com/questions/2450954/how-to-randomize-a-javascript-array
-      var i = divs.length;
-      if ( i == 0 ) return false;
-      while ( --i ) {
-         var j = Math.floor( Math.random() * ( i + 1 ) );
-         var tempi = divs[i];
-         var tempj = divs[j];
-         divs[i] = tempj;
-         divs[j] = tempi;
-         
-       }
-      for(var i = 0; i < divs.length; i++) $(divs[i]).appendTo(this);
-  });   
-
-  });
- });
-
- /*$(function () {
-  $('#apply_stack').on('click', function () {
-
-    var stck = $('#stacks').val()
-    divOrigen.text(stck).css( 'font-weight', 'bold' );
-    divCurr.text("");
-
-    if (stck == "Randomly Shuffled"){
-      shuffle();
-      
-    }
-
-    if (stck == "New Deck Order"){
-      newdeck(0);
-      
-    }
-
-    if (stck == "Mnemonica"){
-      newdeck(2);
-      
-    }
-  });
-});*/
-
-function shuffle() {
-  $(".naipes").each(function(){
-    var divs = $(this).find('div');
-    for(var i = 0; i < divs.length; i++) $(divs[i]).remove();  
-
-    
-    var i = divs.length;
-    if ( i == 0 ) return false;
-    while ( --i ) {
-       var j = Math.floor( Math.random() * ( i + 1 ) );
-       var tempi = divs[i];
-       var tempj = divs[j];
-       divs[i] = tempj;
-       divs[j] = tempi;
-       
-     }
-    for(var i = 0; i < divs.length; i++) $(divs[i]).appendTo(this);
-});   
-}
-
-function newdeck(){
- 
-    var myArray = $(".naipes div").sort(function (a, b) {
-      a = parseInt($(a).attr('id'));
-      b = parseInt($(b).attr('id'));
-      //console.log(a)
-      return a - b;
-      
-    }).each(function() {
-      var elem = $(this);
-      elem.remove();
-      $(elem).appendTo(".naipes");
-    });
-    
-   
-
-}
-
-function sortDom(selectorArray) {
-  
-    for(var i = 1; i <= selectorArray.length; i++){
-      let $el = $(selectorArray[selectorArray.length-i]);
-      $el.parent().prepend($el);
-
-    }
-      
-  
-}
- 
-
-
 var newdeckorder = ['#01', '#02', '#03', '#04', '#05', '#06', '#07', '#08', '#09', '#10', 
 '#11', '#12', '#13', '#14', '#15', '#16', '#17', '#18', '#19', '#20',
 '#21', '#22', '#23', '#24', '#25', '#26', '#27', '#28', '#29', '#30', '#31', '#32', '#33', 
@@ -201,80 +23,170 @@ var newdeckorder1 = ['#13', '#12', '#11', '#10', '#09', '#08', '#07', '#06', '#0
 
 ]
 
+/*
+  FUNCIONES PRIVADAS -------------------------------------------------------------------------------
+
+*/
+
+function fromNaipesToArray (){
+  
+  var arr = new Array();
+  $( ".naipe" ).each(function(index,element) {
+  
+    index1 = index +1
+    var n =  $( ".tapete div:nth-child(" + index1 + ")" ).attr('id')
+    arr.push('#' + n);
+    
+  })
+  
+  return arr
+}
+
+
+function sortDom(selectorArray) {
+  
+  for(var i = 1; i <= selectorArray.length; i++){
+    let $el = $(selectorArray[selectorArray.length-i]);
+    $el.parent().prepend($el);
+
+  }
+}
+
+function checkCurrDeck (){
+  var arr = fromNaipesToArray()
+
+  for (var i = 0; i < arr.length; ++i) {
+    if (arr[i] !== mnemonica[i]) {
+      return;
+    }
+  }   
+  divDestino.text("Mnemonica").css( 'font-weight', 'bold' );
+  
+}
+
+function disableButtons (){
+      $('#cut').prop("disabled",true);
+     $('#invertir').prop("disabled",true);
+     $('#faroExtPar').prop("disabled",true);
+     $('#faroIntPar').prop("disabled",true);
+     $('#faroExt').prop("disabled",false);
+     $('#faroInt').prop("disabled",false);
+     $('#antifaroInt').prop("disabled",false);
+     $('#antifaroExt').prop("disabled",false);
+
+}
+
+function timeoutSelected(){
+  setTimeout(() => {  
+    $(".ui-selected").each(function(index,element) {
+      element.classList.remove("ui-selected");
+    })
+   }, 1500);
+}
+
+     
+/*
+  ORIGIN STACK -----------------------------------------------------------------------------
+
+kinepoli
+stand 
+16:30 a 20:30
+antes de la peli
+brainstorming
+
+
+*/
+
 $('#apply_stack').on('click', function () {
   var stck = $('#stacks').val()
   divOrigen.text(stck).css( 'font-weight', 'bold' );
   divCurr.text("");
-
     
     $(".ui-selected").each(function(index,element) {
       element.classList.remove("ui-selected");
     })
-  
 
     if (stck == "New Deck Order"){
       sortDom(newdeckorder);
-      
     }
 
     if (stck == "Randomly Shuffled"){
       shuffle();
-      
     }
     if (stck == "Mnemonica"){
-      sortDom(mnemonica);
-      
+      sortDom(mnemonica);  
     }
 
     if (stck == "New Deck Order 1"){
       sortDom(newdeckorder1);
-      
     }
 });
-$( function() {
-  $( ".selectable" ).selectable({
-    selected: function() {
-      var num = $(".ui-selected .naipeImg").length
-      
-      if (num>1 && num<52){
-        $('#cut').prop("disabled",false);
-      }
 
-      if(num>1){
-        $('#invertir').prop("disabled",false);
-      }
+/*
+  OPERACIONES -------------------------------------------------------------------------------------
 
-      if (num>1 && (num<26)){
-        $('#faroExtPar').prop("disabled",false);
-        $('#faroIntPar').prop("disabled",false);
-      }
+  Funciones a las que se llama cuando se hace click en un botón
+*/
 
-      if (num>1){
-        $('#faroExt').prop("disabled",true);
-        $('#faroInt').prop("disabled",true);
-      }
+//------------------------------RANDOM SHUFFLE---------------------------
 
-    }
+
+  $('#shuffle').on('click', function () {
+    
+    $(".tapete").each(function(){
+      var divs = $(this).find('div');
+      for(var i = 0; i < divs.length; i++) $(divs[i]).remove();   //Borra el elemento del DOM    
+          
+      //the fisher yates algorithm, from http://stackoverflow.com/questions/2450954/how-to-randomize-a-javascript-array
+      var i = divs.length;
+      if ( i == 0 ) return false;
+      while ( --i ) {
+         var j = Math.floor( Math.random() * ( i + 1 ) );
+         var tempi = divs[i];
+         var tempj = divs[j];
+         divs[i] = tempj;
+         divs[j] = tempi;
+         
+       }
+      for(var i = 0; i < divs.length; i++) $(divs[i]).appendTo(this);
+  });   
+
   });
-  
-} );
-//https://codepen.io/codesnips/pen/QNYoyv
-//http://www.tutorialspark.com/jqueryUI/jQuery_UI_Selectable_Interaction.php
 
 
 
-$(function () {
+//---------------------------- INVERTIR--------------------------
+
+
+  $('#invertir').on('click', function () {
+
+    var num = $(".ui-selected .naipeImg").length
+    var lim = Math.floor(num/2);
+    divCurr.text(divCurr.text() + " --> Inverse " + num + " cards");
+
+    $( ".naipe" ).each(function(index,element) {
+      if (index < lim ){
+        index1 = index + 1
+        $( ".tapete div:nth-child(" + num + ")" ).before( $( ".tapete div:nth-child(" + index1 + ")" ) );
+        $( ".tapete div:nth-child(" + index1 + ")" ).before( $( ".tapete div:nth-child(" + num + ")" ) );
+        num--  
+      }
+    })
+     timeoutSelected()
+     disableButtons()
+     checkCurrDeck()
+
+  });
+
+
+//---------------------------- CORTAR --------------------------
+
+
   $('#cut').on('click', function () {
     var num = $(".ui-selected .naipeImg").length
-  
-    
-    
-    //$(".ui-selected").each(obj, function() {
-    //}
-    
+
     divCurr.text(divCurr.text() + " --> Cut in card " + num);
 
-    
     $( ".naipe" ).each(function(index,element) {
       
       if(index<num){
@@ -283,60 +195,18 @@ $(function () {
       }
     })
 
-    setTimeout(() => {  
-      $(".ui-selected").each(function(index,element) {
-        element.classList.remove("ui-selected");
-      })
-     }, 1500);
-
-     $('#cut').prop("disabled",true);
-     $('#invertir').prop("disabled",true);
-     $('#faroExtPar').prop("disabled",true);
-     $('#faroIntPar').prop("disabled",true);
-     $('#faroExt').prop("disabled",false);
-     $('#faroInt').prop("disabled",false);
+     timeoutSelected()
+     disableButtons()
      checkCurrDeck()
 
   });
-});
-
-function checkCurrDeck (){
-  var arr = fromNaipesToArray()
-  console.log(arr)
-  console.log(mnemonica)
-  
-  for (var i = 0; i < arr.length; ++i) {
-    if (arr[i] !== mnemonica[i]) {
-      console.log("Ocurrió lo inesperado: " + i)
-      console.log(arr[i])
-      console.log(mnemonica[i])
-      return;
-    }
-  }
-  
-    
-  divDestino.text("Mnemonica").css( 'font-weight', 'bold' );
-  
-
-}
-
-function fromNaipesToArray (){
-  
-  var arr = new Array();
-  $( ".naipe" ).each(function(index,element) {
-    index1 = index +1
-    
-   var n =  $( ".tapete div:nth-child(" + index1 + ")" ).attr('id')
-   arr.push('#' + n);
-    
-  })
-  
-  return arr
-}
 
 
-$(function () {
+//---------------------------- FARO EXTERIOR --------------------------
+
+
   $('#faroExt').on('click', function () {
+
     divCurr.text(divCurr.text() + " --> Faro Exterior");
     var arr = fromNaipesToArray()
     var half = arr.slice(0,26);
@@ -350,9 +220,12 @@ $(function () {
     sortDom(arr)
     
   });
-});
 
-$(function () {
+
+
+
+//---------------------------- FARO INTERIOR --------------------------
+
   $('#faroInt').on('click', function () {
     divCurr.text(divCurr.text() + " --> Faro Interior");
     var arr = fromNaipesToArray()
@@ -367,9 +240,13 @@ $(function () {
     sortDom(arr)
     
   });
-});
 
-$(function () {
+
+
+
+//---------------------------- FARO EXTERIOR PARCIAL --------------------------
+
+
   $('#faroExtPar').on('click', function () {
     
 
@@ -380,27 +257,24 @@ $(function () {
     var half = arr.slice(0,num);
     var otherhalf = arr.slice(num,53);
 
-    console.log(half)
-    console.log(otherhalf)
-
     for (var i = 0; i < num; i++) {
       arr[2*i] = half[i]
       arr[2*i+1] = otherhalf[i]
     }
 
-    setTimeout(() => {  
-      $(".ui-selected").each(function(index,element) {
-        element.classList.remove("ui-selected");
-      })
-     }, 1500);
+    timeoutSelected()
 
-    
+    disableButtons()
     sortDom(arr)
     
   });
-});
 
-$(function () {
+
+
+
+//---------------------------- FARO INTERIOR PARCIAL--------------------------
+
+
   $('#faroIntPar').on('click', function () {
 
     var num = $(".ui-selected .naipeImg").length
@@ -410,27 +284,158 @@ $(function () {
     var half = arr.slice(0,num);
     var otherhalf = arr.slice(num,53);
 
-    console.log(half)
-    console.log(otherhalf)
-
     for (var i = 0; i < num; i++) {
       arr[2*i] = otherhalf[i]
       arr[2*i+1] = half[i]
     }
 
-    setTimeout(() => {  
-      $(".ui-selected").each(function(index,element) {
-        element.classList.remove("ui-selected");
-      })
-     }, 1500);
-
-    
+    timeoutSelected()
+    disableButtons()
     sortDom(arr)
     
   });
+
+
+
+//---------------------------- ANTIFARO EXT--------------------------
+
+  $('#antifaroExt').on('click', function () {
+
+    divCurr.text(divCurr.text() + " --> Antifaro");
+    var newarr = new Array();
+    var arr = fromNaipesToArray()
+
+    for (var i = 0; i < (arr.length/2); i++) {
+      newarr.push(arr[2*i])
+    }
+
+    for (var i = 0; i < (arr.length/2); i++) {
+      newarr.push(arr[2*i+1])
+    }
+  
+    sortDom(newarr)
+    
+  });
+
+//---------------------------- ANTIFARO INT--------------------------
+
+$('#antifaroInt').on('click', function () {
+
+  divCurr.text(divCurr.text() + " --> Antifaro");
+  var newarr = new Array();
+  var arr = fromNaipesToArray()
+
+  for (var i = 0; i < (arr.length/2); i++) {
+    newarr.push(arr[2*i +1])
+  }
+
+  for (var i = 0; i < (arr.length/2); i++) {
+    newarr.push(arr[2*i])
+  }
+
+  sortDom(newarr)
+  
 });
 
+//---------------------------- CLEAR LAST OPERATION --------------------------
 
+$('#clearLast').on('click', function () {
+
+  divCurr.text(divCurr.text() + " --> Antifaro");
+  var newarr = new Array();
+  var arr = fromNaipesToArray()
+
+  for (var i = 0; i < (arr.length/2); i++) {
+    newarr.push(arr[2*i +1])
+  }
+
+  for (var i = 0; i < (arr.length/2); i++) {
+    newarr.push(arr[2*i])
+  }
+
+  sortDom(newarr)
+  
+});
+
+/*
+  BOTONES CURRENT COMBINATION ------------------------------------------------------------
+
+*/
+$(function () {
+   $('#clear').on('click', function () {
+     divCurr.text("");
+   });
+ });
+ 
+
+/*
+  SELECTABLE ------------------------------------------------------------
+//https://codepen.io/codesnips/pen/QNYoyv
+//http://www.tutorialspark.com/jqueryUI/jQuery_UI_Selectable_Interaction.php
+*/
+
+$( function() {
+  $( ".selectable" ).selectable({
+    selected: function() {
+      var num = $(".ui-selected .naipeImg").length
+      
+      if (num>0 && num<52){
+        $('#cut').prop("disabled",false);
+      }
+
+      if (num>=52){
+        $('#cut').prop("disabled",true);
+      }
+
+      if(num>1){
+        $('#invertir').prop("disabled",false);
+      }
+
+      if (num>1 && (num<26)){
+        $('#faroExtPar').prop("disabled",false);
+        $('#faroIntPar').prop("disabled",false);
+      }
+
+      if (num>=26){
+        $('#faroExtPar').prop("disabled",true);
+        $('#faroIntPar').prop("disabled",true);
+      }
+
+      if (num>1){
+        $('#faroExt').prop("disabled",true);
+        $('#faroInt').prop("disabled",true);
+        $('#antifaroExt').prop("disabled",true);
+        $('#antifaroInt').prop("disabled",true);
+      }
+
+      if (num==0){
+        disableButtons()
+      }
+
+    }
+
+    
+  });
+  
+} );
+
+$( function() {
+  $( ".selectable" ).selectable({
+    unselected: function() {
+      var num = $(".ui-selected .naipeImg").length
+
+      if (num==0){
+        disableButtons()
+      }
+
+    }
+
+  });
+  
+} );
+
+
+//Nada más empezar...
 sortDom(['#01', '#02', '#03', '#04', '#05', '#06', '#07', '#08', '#09', '#10', 
   '#11', '#12', '#13', '#14', '#15', '#16', '#17', '#18', '#19', '#20',
   '#21', '#22', '#23', '#24', '#25', '#26', '#27', '#28', '#29', '#30', '#31', '#32', '#33', 
@@ -444,5 +449,38 @@ sortDom(['#01', '#02', '#03', '#04', '#05', '#06', '#07', '#08', '#09', '#10',
   $('#faroExt').prop("disabled",false);
   $('#faroInt').prop("disabled",false);
 
-  //Cuando deselecciono no se disabled!!s
-  //check current se borra mnemonica! Tras el cambio ya solo se puede usar una vez??
+  
+  
+/*
+FUNCIONES ANTIGUAS
+
+function newdeck(){
+ 
+    var myArray = $(".naipes div").sort(function (a, b) {
+    a = parseInt($(a).attr('id'));
+    b = parseInt($(b).attr('id'));
+    
+    return a - b;
+      
+    }).each(function() {
+      var elem = $(this);
+      elem.remove();
+      $(elem).appendTo(".naipes");
+    });
+
+}
+
+$(function () {
+  $('#change').on('click', function () {
+    var num = parseInt($('#cambiadas').val());
+    var num1 = parseInt($('#cambiadas1').val()) + 1;
+
+    
+    divCurr.text(divCurr.text() + " --> Put card " + num + " in position " + num1);
+
+    $( ".tapete div:nth-child(" + num1 + ")" ).before( $( ".tapete div:nth-child(" + num + ")" ) );
+    
+  });
+});
+
+*/
